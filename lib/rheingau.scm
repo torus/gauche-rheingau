8d502f13c859e838c634d292e2e2329c0aa5ff70
Toru Hisai <toru@torus.jp>
2018-04-29 19:21:32 +0200

    modulize

---
+++ lib/rheingau.scm
@@ -0,0 +1,52 @@
+(define-module rheingau
+  (use rfc.http)
+  (use rfc.json)
+  (use srfi-11)
+  (use file.util)
+  (use util.match)
+
+  (export rheingau-install rheingau-use)
+  )
+
+(select-module rheingau)
+
+(define (rheingau-install pkg)
+  (define dest-dir (build-path "gosh-modules" pkg))
+
+  (unless (file-type dest-dir)
+
+    (print #`"Installing ,pkg")
+
+    ;; https://stackoverflow.com/questions/46762667/firebase-firestore-rest-example
+    (let* ((server "firestore.googleapis.com")
+           (project "project-6711518500697978711")
+           (uri #`"/v1beta1/projects/,|project|/databases/(default)/documents/rheingau/,|pkg|"))
+      (let-values (((status header body) (http-get server uri :secure #t)))
+        (unless (string=? status "200") (error #`"Couldn't find the package: ,|pkg|."))
+
+        (let* ((port (open-input-string body))
+               (data (parse-json port))
+               (fields (cdr (assoc "fields" data string=?)))
+               (repo-elem (cdr (assoc "repo" fields string=?)))
+               (repo (cdr (assoc "stringValue" repo-elem string=?))))
+
+          (let ((script-path #f))
+            (create-directory-tree
+             "gosh-modules"
+             `(".build"
+               ((,pkg
+                 (("build.sh"
+                   ,(lambda (path)
+                      (print #`"git clone ,repo ,dest-dir")
+                      (set! script-path path))))))))
+            (sys-system #`"sh ,script-path")))))))
+
+(define-syntax rheingau-use
+  (er-macro-transformer
+    (^[form rename id=?]
+      (match form
+        [(_ pkg)
+         `(begin (add-load-path ,(build-path "." "gosh-modules" (symbol->string pkg)))
+                 (use ,pkg)
+                 )]
+        [_ (error "malformed rheingau-use:" form)]))))
--- rheingau.scm
+++ rheingau.scm
@@ -1,8 +1,5 @@
-(use gauche.parseopt)
-(use rfc.http)
-(use rfc.json)
-(use srfi-11)
-(use file.util)
+(add-load-path "./lib")
+(use rheingau)
 
 ;; Command line interface
 (define (main args)
@@ -10,35 +7,4 @@
     (case (string->symbol command)
       ((install i)
        (let ((pkg (caddr args)))
-         (install pkg))))))
-
-(define (install pkg)
-  (define dest-dir (build-path "gosh-modules" pkg))
-
-  (unless (file-type dest-dir)
-
-    (print #`"Installing ,pkg")
-
-    ;; https://stackoverflow.com/questions/46762667/firebase-firestore-rest-example
-    (let* ((server "firestore.googleapis.com")
-           (project "project-6711518500697978711")
-           (uri #`"/v1beta1/projects/,|project|/databases/(default)/documents/rheingau/,|pkg|"))
-      (let-values (((status header body) (http-get server uri :secure #t)))
-        (unless (string=? status "200") (error #`"Couldn't find the package: ,|pkg|."))
-
-        (let* ((port (open-input-string body))
-               (data (parse-json port))
-               (fields (cdr (assoc "fields" data string=?)))
-               (repo-elem (cdr (assoc "repo" fields string=?)))
-               (repo (cdr (assoc "stringValue" repo-elem string=?))))
-
-          (let ((script-path #f))
-            (create-directory-tree
-             "gosh-modules"
-             `(".build"
-               ((,pkg
-                 (("build.sh"
-                   ,(lambda (path)
-                      (print #`"git clone ,repo ,dest-dir")
-                      (set! script-path path))))))))
-            (sys-system #`"sh ,script-path")))))))
+         (rheingau-install pkg))))))
